d601194cefd9833344d53a38be728feafa18cef6
shnayder.1206 <shnayder.1206@gmail.com>
2024-03-04 00:11:02 +0700

    подключение api. настройка макетов

--- app/src/main/java/com/shnayder/android/wewatch/SearchActivity.kt
+++ app/src/main/java/com/shnayder/android/wewatch/SearchActivity.kt
@@ -1,17 +1,69 @@
 package com.shnayder.android.wewatch
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import com.shnayder.android.wewatch.retrofit.Film
+import com.shnayder.android.wewatch.retrofit.FilmApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-
-class SearchActivity : AppCompatActivity()  {
+class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.search_film)
-        val addFilm = findViewById<Button>(R.id.add_film)
-        addFilm.setOnClickListener{
+        setContentView(R.layout.activity_search)
 
+        val searchFilm = findViewById<Button>(R.id.search_film)
+        val miniSearchFilm = findViewById<ImageView>(R.id.mini_search_film)
+        val enterTitleFilm = findViewById<EditText>(R.id.enter_title_film)
+        val enterYearFilm = findViewById<EditText>(R.id.enter_year_film)
+
+        searchFilm.setOnClickListener{
+            val title = enterTitleFilm.text.toString()
+            val year = enterYearFilm.text.toString()
+
+            if(title != "" && year!=""){
+                val intent = Intent(this@SearchActivity, AllFilmActivity::class.java)
+                intent.putExtra("title", title)
+                intent.putExtra("year", year)
+                startActivity(intent)
+            } else if(title != ""){
+                val intent = Intent(this@SearchActivity, AllFilmActivity::class.java)
+                intent.putExtra("title", title)
+                startActivity(intent)
+            } else
+                Toast.makeText(this@SearchActivity, "Для поиска введите название фильма.", Toast.LENGTH_SHORT).show()
         }
+
+
+
+
+
+
+
+
+
+
+
+
+        miniSearchFilm.setOnClickListener{
+            val title = enterTitleFilm.text.toString()
+            if(title != ""){
+                val intent = Intent(this@SearchActivity, AllFilmActivity::class.java)
+                intent.putExtra(title, title)
+                startActivity(intent)
+            } else
+                Toast.makeText(this@SearchActivity, "Для поиска введите название фильма.", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
--- app/src/main/java/com/shnayder/android/wewatch/retrofit/FilmApi.kt
+++ app/src/main/java/com/shnayder/android/wewatch/retrofit/FilmApi.kt
@@ -1,11 +1,16 @@
 package com.shnayder.android.wewatch.retrofit
 
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 /*интерфейс продукт api
 (прописываем запросы, которые будет отправлять retrofit/
 GET-получить)*/
+
 interface FilmApi {
-    @GET("?i=tt3896198&apikey=e451f001")
-    fun getFilmById():Film
+    @GET("/")
+    suspend fun getFilmByTitle(@Query("apikey") apikey: String, @Query("s") title: String):Film
+    @GET("/")
+    suspend fun getFilmByTitleAndYear(@Query("apikey") apikey: String, @Query("s") title: String, @Query("y") year: String):Film
 }
